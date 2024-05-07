e388ce061e9d2dd4fd556a6a70b94675b39bfd59
tastynoodles4me <167091295+tastynoodles4me@users.noreply.github.com>
2024-05-07 17:06:54 +1000

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     const DIR = {
-        email: 'YOUR EMAIL',  // replace YOUR EMAIL with your email for auto login (keep everything else the same)
-        password: 'YOUR PASSWORD',  // replace YOUR PASSWORD with your password for auto login
+        email: 'akio.cheong@education.nsw.gov.au',  // replace YOUR EMAIL with your email for auto login (keep everything else the same)
+        password: 'whyuhackingme',  // replace YOUR PASSWORD with your password for auto login
 
         login_url: 'https://app.educationperfect.com/app/login',
 
